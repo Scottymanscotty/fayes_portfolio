5d273662242ce93623bd5c406b2132875a23ac78
Scott Walton <scottymanscotty@gmail.com>
2016-02-24 16:36:07 +0000

    added external javascript file and linked in html

---
+++ javascript/javascript.js
@@ -0,0 +1,26 @@
+   $(function(){
+      $('#menu').slicknav({
+        closeOnClick:true
+      });
+    });
+     $(window).load(function () {
+        $('.Collage').collagePlus();
+
+         function collage() {
+    $('.Collage').collagePlus(
+        {
+            'fadeSpeed' : 2000
+        }
+    );
+};
+
+var resizeTimer = null;
+$(window).bind('resize', function() {
+    // hide all the images until we resize them
+    // set the element you are scaling i.e. the first child nodes of ```.Collage``` to opacity 0
+    $('.Collage .Image_Wrapper').css("opacity", 0);
+    // set a timer to re-apply the plugin
+    if (resizeTimer) clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(collage, 200);
+});
+    });
