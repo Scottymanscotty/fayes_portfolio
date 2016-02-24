193a3b6a8fef56dfe07621cacf9079cb120f8d65
Scott Walton <scottymanscotty@gmail.com>
2016-02-24 17:31:12 +0000

    added javascript for lightbox view on gallery

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,8 +1,12 @@
-   $(function(){
+//SLICKNAV JS
+
+$(function(){
       $('#menu').slicknav({
         closeOnClick:true
       });
     });
+
+// COLLAGE JS
      $(window).load(function () {
         $('.Collage').collagePlus();
 
@@ -23,4 +27,43 @@ $(window).bind('resize', function() {
     if (resizeTimer) clearTimeout(resizeTimer);
     resizeTimer = setTimeout(collage, 200);
 });
+
+var $overlay = $("<div id='overlay'></div>");
+var $image = $("<img>");
+var $caption = $("<p></p>");
+
+// an image
+
+$overlay.append($image);
+
+
+  // a caption
+$overlay.append($caption);
+// Add Overlay
+ $("body").append($overlay);
+
+//.capture the click event on an a link to an image
+$(".Collage a").click(function(event) {
+  event.preventDefault();
+  var imageLocation = $(this).attr("href");
+  // update overlay with the image linked in the link.
+  $image.attr("src", imageLocation);
+  // show the overlay
+  $overlay.show();
+
+  // Get child's alt attribute at set caption.
+  var captionText = $(this).children("img").attr("alt");
+  $caption.text(captionText);
+});
+
+
+// When overlay is clicked
+  // Hide the overlay.
+
+$overlay.click(function(){
+  $overlay.hide();
+});
+
+
     });
+
