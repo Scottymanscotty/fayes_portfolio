f5f6092cad1351357d5f2437d0f514c1804ee9c6
Scott Walton <scottymanscotty@gmail.com>
2016-04-11 15:38:20 +0100

    removed overlay js to another file

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -28,42 +28,6 @@ $(window).bind('resize', function() {
     resizeTimer = setTimeout(collage, 200);
 });
 
-var $overlay = $("<div id='overlay'></div>");
-var $image = $("<img>");
-var $caption = $("<p></p>");
-
-// an image
-
-$overlay.append($image);
-
-
-  // a caption
-$overlay.append($caption);
-// Add Overlay
- $("body").append($overlay);
-
-//.capture the click event on an a link to an image
-$(".Collage a").click(function(event) {
-  event.preventDefault();
-  var imageLocation = $(this).attr("href");
-  // update overlay with the image linked in the link.
-  $image.attr("src", imageLocation);
-  // show the overlay
-  $overlay.show();
-
-  // Get child's alt attribute at set caption.
-  var captionText = $(this).children("img").attr("alt");
-  $caption.text(captionText);
-});
-
-
-// When overlay is clicked
-  // Hide the overlay.
-
-$overlay.click(function(){
-  $overlay.hide();
-});
-
 
     });
 
